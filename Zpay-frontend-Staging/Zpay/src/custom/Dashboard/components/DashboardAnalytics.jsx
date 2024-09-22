4d72c63a9d3f4a7a4c09fa02f7be4740c4cbfae7
Kiran Kumar <kirankumar@Kiran-Kumars-MacBook-Air.local>
2024-09-22 08:16:57 +0530

    minor-changes

--- Zpay-frontend-Staging/Zpay/src/custom/Dashboard/components/DashboardAnalytics.jsx
+++ Zpay-frontend-Staging/Zpay/src/custom/Dashboard/components/DashboardAnalytics.jsx
@@ -1,7 +1,9 @@
 //this is the code for the dashboard analytics page
 import "./card.css"
-import { LineChart } from '@mui/x-charts/LineChart';
 import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+
 const DashboardAnalytics=()=>{
   return (
     <div>
@@ -9,32 +11,45 @@ const DashboardAnalytics=()=>{
     <div className="flex flex-row  p-6 gap-60">
      
         {/* Additional dashboard content can be placed here */}
-        <div className="analytics-card">
-          <h2>Analytics Card</h2>
+        <div className="analytics-card flex justify-center space-betweenw-44 h-24">
+          <div>
+          <h2 className="bg-blend-color-dodge">Analytics Card</h2>
+          </div>
         </div>
-        <div className="analytics-card">
+
+        <div className="analytics-card flex justify-center space-betweenw-44 h-24">
           <h2>No of Users</h2>
         </div>
       
     </div>
-    <div className="flex flex-col justify-start h-96">
-       <BasicLineChart />
+    <div className="flex flex-col  justify-start h-96">
+      <Chart/>
        </div>
     </div>
   );
 }
 
-export function BasicLineChart() {
+
+
+export function Chart() {
+   const dataset = [
+    { x: 1, y: 2 },
+    { x: 2, y: 5.5 },
+    { x: 3, y: 2 },
+    { x: 5, y: 8.5 },
+    { x: 8, y: 1.5 },
+    { x: 10, y: 5 },
+  ];
+  
   return (
     <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      width={500}
+      dataset={dataset}
+      xAxis={[{ dataKey: 'x' }]}
+      series={[{ dataKey: 'y' }]}
       height={300}
+      width={900}
+      margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+      grid={{ vertical: false, horizontal: true }}
     />
   );
 }
